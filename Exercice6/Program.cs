8a611fc2b59194a559cc3ca1be7b2312d8beeb0f
remi <remidebruyne@hotmail.com>
2023-12-20 13:27:26 +0100

    add exercices 6/7/8

---
+++ Exercice6/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("--- Calcul du périmètre et de l'aire d'un carré --- \n" +
+    "Entrez la longueur d'un  coté du carré (en cm) :");
+string squareSide = Console.ReadLine();
+Console.WriteLine($"Le périmètre du carré est : {int.Parse(squareSide) * 4}");
+Console.WriteLine($"L'aire du carré est : {int.Parse(squareSide) * int.Parse(squareSide)}");
+
+
+
---
+++ Exercice7/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- Calcul de la longueur de l'hypothnéuse --- \n" +
+    "Entrez la longueur du premier coté (en cm) :");
+string premierCoteChain = Console.ReadLine();
+double premierCote = double.Parse(premierCoteChain);
+
+Console.WriteLine("Entrez la longueur du premier coté (en cm)");
+string deuxiemeCoteChain = Console.ReadLine();
+double deuxiemeCote = double.Parse(deuxiemeCoteChain);
+
+Console.WriteLine($"La longueur de l'hypthénuse est : {Math.Pow(premierCote, 2) + Math.Pow(deuxiemeCote, 2)}");
