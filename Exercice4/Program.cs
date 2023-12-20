854eb9a08c12f782019787eae42eccc5cd428992
Administrateur <Administrateur@LIL-HFRY1L2>
2023-12-20 11:31:20 +0100

    Ajoutez des fichiers projet.

---
+++ Exercice4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Veuillez saisir votre nom : ");
+string name = Console.ReadLine();
+Console.WriteLine("Veuillez saisir votre prénom : ");
+string surname = Console.ReadLine();
+Console.WriteLine("Veuillez saisir votre âge ");
+string age = Console.ReadLine();
+Convert.ToInt32(age, 10);
+Console.WriteLine($"Bonjour {surname} {name}, vous avez {age} ans");
+
+
+
---
+++ Exercice5/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Veuillez saiair un nombre : ");
+string numA = Console.ReadLine();
+
+Console.WriteLine("Veuillez saiair un nombre : ");
+String numB = Console.ReadLine();
+
+int result = int.Parse(numA) + Convert.ToInt32(numB); //both solution to convert readline() from string to int
+Console.WriteLine($"La somme de ces deux nombres est : {result}");
