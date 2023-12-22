6347cb38331b2f22f426892e0313b264bf9955ff
remi <remidebruyne@hotmail.com>
2023-12-22 13:28:28 +0100

    add exercices 22/23/24

--- Exercice22-bonus/Program.cs
+++ Exercice22-bonus/Program.cs
@@ -1,21 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Channels;
+
 Console.WriteLine("--- Les tables de multipliplication");
 Console.WriteLine();
 
 Console.WriteLine("Combien de table voulez vous");
 int tables = int.Parse(Console.ReadLine());
 
+
+
 for (int i = 1; i <= tables; i++)
 {
-    Console.WriteLine("\n+---+---+---+---+---+---+---+---+---+---+");
+    Console.WriteLine(string.Concat(Enumerable.Repeat("+---", 10)) + "+");
+
     for (int j = 1; j <= 10; j++)
     {
-        if (i * j < 10)
-            Console.Write($"  {i * j}|");
-        else if(i * j > 10);
-        Console.Write($" {i * j}|");
+
+            Console.Write($"|{i * j,3}");
+
     }
+    Console.WriteLine("|");
 }
+Console.WriteLine(string.Concat(Enumerable.Repeat("+---", 10)) + "+");
+
+
 
 /*for (int i = 1; i <= 10; i++)
 {
--- Exercice24/Program.cs
+++ Exercice24/Program.cs
@@ -1,14 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-int sum = 0;
+using System.Diagnostics.CodeAnalysis;
+/*if (!int.TryParse(Console.ReadLine(), out int num))
+    Console.WriteLine("Valeur inccorecte");
+else
+    for (int i = 1; i < num / 2 + 1; i++)
+    {
+        int sum = 0;
+        for (int j = i; j < num; j++)
+        {
+            sum += j;
+            if (sum == num)
+            {
+                string affichage = $"{num} = {i}";
+                Console.Write(affichage);
+                for (int k = i + 1; k <= j; k++)
+                {
+                    Console.Write($" + {k}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }*/
 
 if (!int.TryParse(Console.ReadLine(), out int num))
     Console.WriteLine("Valeur inccorecte");
 else
-    for (int i = 0; i < length; i++)
+    for (int i = 1; i < num / 2 + 1; i++)
     {
-        sum
-        Console.WriteLine($"{num} = ");
+        int sum = 0;
+        string chain = "";
+        for (int j = i; j < num; j++)
+        {
+            sum += j;
+            chain += j + "+";
+           // Console.Write(affichage);
+
+            if (sum == num)
+            {
+                Console.Write(num + " = " + chain.TrimEnd('+'));
+                Console.WriteLine();
+            }
+        }
     }
-    
