f979808febaf2a3682499a26a97d4b6911a81d83
Remi Debruyne <remidebruyne@hotmail.com>
2024-01-02 16:00:02 +0100

    add exercice33

---
+++ Exercice33/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] tableau = new int[26];
+int ascii = 65;
+
+for (int i = 0; i < tableau.Length; i++)
+{
+    tableau[i] = ascii;
+    ascii++;
+}
+
+int espace = 0;
+for (int i = 0; i < tableau.Length; i++)
+{
+    Console.WriteLine(string.Concat(Enumerable.Repeat(" ", espace)) + (char)tableau[i]);
+    espace++;
+}
