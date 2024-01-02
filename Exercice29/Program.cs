a024426a4ea64a6a4bbe944b9ff0968b32d4937b
Remi Debruyne <remidebruyne@hotmail.com>
2024-01-02 13:34:37 +0100

    finish exercice 29

--- Exercice29/Program.cs
+++ Exercice29/Program.cs
@@ -3,16 +3,18 @@ using System.Net.NetworkInformation;
 
 Console.WriteLine("--- Gestion des notes ---");
 
-int nb = 0;
-int note = 0;
+float max = 20, min = 0, moyenne;
+int nbNotes = 1;
+int userInput = 0;
 
-do
+Console.WriteLine($"Veuillez saisir les notes : " +
+   $"\n(999 pour calculer)");
+
+while ( userInput != 999)
 {
-    Console.WriteLine($"Veuillez saisir les notes : " +
-        $"(999 pour calculer)");
-    Console.WriteLine($"\t - Merci de saisir la note {nb++} : {note = int.Parse(Console.ReadLine())} ");
-    
-    if (note > 20)
+    Console.WriteLine($"\t - Merci de saisir la note {nb++} (sur /20) : {userInput = int.Parse(Console.ReadLine())} ");
+
+    if (userInput > 20 && userInput != 999)
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Erreur de saisie, la note est supérieur à 20 !");
@@ -20,5 +22,3 @@ do
 
     }
 }
-
-while ( note != 999 );
