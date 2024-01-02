4035c80d10ddd37497bf5a60e6a35d05add4e020
Remi Debruyne <remidebruyne@hotmail.com>
2024-01-02 16:52:50 +0100

    start exercice 36

---
+++ Exercice34/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Est pair ou Impair ?");
+Console.WriteLine("Combiens de nombre contiendra le tableau ? : ");
+Random rnd = new Random();
+int num = 0;
+int[] tableau = new int[num];
+
+
+if (!int.TryParse(Console.ReadLine(), out  num))
+    Console.WriteLine("Valeur inccorecte");
+else
+{
+    for (int i = 0; i < tableau.Length; i++)
+    {
+        tableau[i] = rnd.Next(1, 51);
+    }
+}
+
+
+
---
+++ Exerecice36/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- Tableau des notes ---");
+Console.WriteLine("Combien de note comportera le tableau ? : ");
+
+int.TryParse(Console.ReadLine(), out int nombreDeNote);
+int[] notes = new int[nombreDeNote];
+
+for (int i = 0; i < notes.Length; i++)
+{
+    Console.WriteLine($"\t- Note {i} :  ");
+    notes[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("--- Liste des notes ---");
+for (int i = 0;i < notes.Length; i++)
+{
+    Console.WriteLine($"La note {i} est : {notes[i]}/20");
+}
