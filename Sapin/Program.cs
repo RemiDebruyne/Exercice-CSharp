cf0fa0ed0b9bac00fdc2c654c38b028213a9e3f4
Remi Debruyne <remidebruyne@hotmail.com>
2024-01-02 10:12:23 +0100

    add sapin exercice

---
+++ Sapin/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("De quelle taille doit être le sapin");
+Console.WriteLine("De quelle taille doit être le tronc");
+
+if (!int.TryParse(Console.ReadLine(), out int taille))
+    Console.WriteLine("Valeur inccorecte");
+else
+{
+    if (!int.TryParse(Console.ReadLine(), out int tronc))
+    {
+        Console.WriteLine("valeur incorrecte");
+    }
+    else
+    {
+
+        int etoile = 1;
+        int espace = taille;
+        for (int i = 1; i <= taille; i++)
+        {
+            Console.Write(string.Concat(Enumerable.Repeat(" ", espace - 1)));
+            espace--;
+            Console.WriteLine(string.Concat(Enumerable.Repeat("*", etoile)));
+            etoile++;
+            etoile++;
+        }
+        for (int i = 0; i < tronc; i++)
+        {
+            Console.Write(string.Concat(Enumerable.Repeat(" ", taille - 2)));
+            Console.WriteLine("| |");
+        }
+    }
+}
+
+
+
+
