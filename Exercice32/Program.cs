7a76ec4dbefa836a925815a347e23ccd48879b16
Remi Debruyne <remidebruyne@hotmail.com>
2024-01-02 15:46:35 +0100

    add exercice 32

---
+++ Exercice32/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Insertion des valeurs dans le tableau : ");
+
+string[] tableau = new string[10];
+
+for (int i = 0; i < tableau.Length; i++)
+{
+    Console.Write("saisir la valeur " + (i + 1) + " : ");
+    tableau[i] = Console.ReadLine();
+}
+
+int espace = 0;
+for (int i = 0; i < tableau.Length; i++)
+{
+    Console.WriteLine(string.Concat(Enumerable.Repeat("\t", espace)) + tableau[i]);
+    espace++;
+}
