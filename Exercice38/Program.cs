a8dad71b25fc6dd54143d7a91929111b5e69cd98
Remi Debruyne <remidebruyne@hotmail.com>
2024-01-03 12:14:33 +0100

    add exercice38

---
+++ Exercice38/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- Où est passé mon numéro ---");
+
+Console.WriteLine("Affectation des valeurs...");
+
+Random rdn = new Random();
+int[] arr = new int[10];
+string espace = "";
+
+
+for (int i = 0; i < arr.Length; i++)
+    arr[i] = rdn.Next(1, 51);
+
+Console.WriteLine("Affichage avant le triage");
+foreach (int i in arr)
+{
+    Console.WriteLine(espace + i);
+    espace += " ";
+}
+
+int firstNum = arr[0];
+
+
+Console.WriteLine("\nAffichae après le triage");
+Array.Sort(arr);
+espace = "";
+
+foreach(int i in arr)
+{
+    Console.WriteLine(espace + i);
+    espace += " ";
+}
+
+
+Console.WriteLine($"\nLe nombre {firstNum} est à la {Array.IndexOf(arr, firstNum)} position");
